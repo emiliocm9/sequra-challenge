5e7e66d717125821d8e12a95648434eb7c69cf74
Emilio Contreras <emilio.contreras97@gmail.com>
2022-05-27 05:24:49 -0500

    add data seeders for shoppers, orders and merchants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+# load seed data from fixtures/merchants.csv for Merchant model
+#
+CSV.foreach('fixtures/merchants.csv') do |row|
+  Merchant.create(
+    name: row[1],
+    email: row[2],
+    cif: row[3]
+  )
+end
+
+# load seed data from fixtures/shoppers.csv for Shopper model
+#
+CSV.foreach('fixtures/shoppers.csv') do |row|
+  Shopper.create(
+    name: row[1],
+    email: row[2],
+    nif: row[3]
+  )
+end
+
+# load seed data from fixtures/orders.csv for Order model
+#
+CSV.foreach('fixtures/orders.csv') do |row|
+  order = Order.new(
+    amount: Money.new((row[3].to_f * 100).round),
+    created_at: row[4],
+    completed_at: row[5],
+  )
+  order.merchant = Merchant.find(row[1])
+  order.shopper = Shopper.find(row[2])
+  order.save
+end
+
